aee32aaf3004bf13cb7a71e542ea7c4a19219825
Kris <krischiang0301@gmail.com>
2022-11-04 17:33:25 +0800

    feat: install mongoose

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const app = express()
 const http = require('http')
 const server = http.createServer(app)
 const PORT = 3000
-
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+// Models setting
+require('./config/mongoose')
+// Views setting
 const exphbs = require('express-handlebars')
-
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 

99f92b090c2be08c0279b857feb10ce5b7438fa9
Kris <krischiang0301@gmail.com>
2022-11-04 23:10:41 +0800

    feat: add socket

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
-const { engine } = require('express-handlebars')
 const app = express()
+const exphbs = require('express-handlebars')
+const routes = require('./routes/index.js')
 const http = require('http')
 const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
+
 const PORT = 3000
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -10,14 +14,25 @@ if (process.env.NODE_ENV !== 'production') {
 // Models setting
 require('./config/mongoose')
 // Views setting
-const exphbs = require('express-handlebars')
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-app.get('/', (req, res) => {
-  res.render('index')
+app.use(express.static('publics'))
+
+app.use(routes)
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.broadcast.emit('hi')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg)
+    io.emit('chat message', msg)
+  })
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`)
 })
